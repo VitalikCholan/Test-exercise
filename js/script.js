6663c767d5e88dfc67e916ad51adb02f5a85586a
Віталій Чолан <vitalikcolan@gmail.com>
2024-03-02 09:19:16 +0200

    Added small changes and comments

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
+// Даний Event Listener очікує на повну загрузку DOM перед виконанням вказаної функції
 document.addEventListener('DOMContentLoaded', function() {
+    // Отримую елемент 'analyzeButton' з DOM
     const analyzeButton = document.getElementById('analyzeButton');
+    // Додаю подію 'click' для кнопки 'Analyze', цим самим активовую функцію 'handleFile' під час кліку
     analyzeButton.addEventListener('click', handleFile);
 })
 
+// Функція для введення файлу та проведення аналізу
 function handleFile() {
+    // Отримую елементи 'fileInput' та 'results' з DOM
     const fileInput = document.getElementById('fileInput');
     const resultsDiv = document.getElementById('results');
 
+    // Перевіряю, чи вибрали файл
     if(!fileInput.files || fileInput.files.length === 0) {
-        resultsDiv.innerText = 'Please select  a file';
-        return;
+        // Якщо ні, то користувачу виводяться дане повідомлення
+        return resultsDiv.innerText = 'Please select  a file';
     }
 
+    // Отримую перший файл з вибраних файлів
     const file = fileInput.files[0];
+    // Додаю об'єкт 'FileReader'
     const reader = new FileReader();
 
+    // Даний об'єкт зчитує вміст файлу як текст
     reader.readAsText(file);                        
 
-    reader.onload = function(event) {
-        const content = event.target.result;
+    // Додаю Event Listener для аналізу завантаженого файлу
+    reader.addEventListener('load', e => {
+        // Отримую вміст файлу
+        const content = e.target.result;
 
+        // Ділю вміст файла за символами нового рядка та перетворюю кожний рядок у число
         const numbers = content.split('\n').map(Number);
 
+        // Відфільтровую нечислові значення та порожні рядки
         const filteredNumbers = numbers.filter(num => !isNaN(num) && num !== '');
 
+        // Створюю змінні для максимального та мінімального числа
         let maxNumber = Number.MIN_SAFE_INTEGER;
         let minNumber = Number.MAX_SAFE_INTEGER;
 
+        // Через цикл знаходжу максимальні та мінімальні числа
         for(const num of filteredNumbers) {
             if (num > maxNumber){
                 maxNumber = num;
@@ -37,19 +52,26 @@ function handleFile() {
             }
         }
 
+        // Сортую відфільтровані числа за зростанням
         const sortedNumbers = filteredNumbers.sort((a, b) => a - b);
+
+        // Знаходжу медіану за допомогою математичних обчислень та тернарного оператора
         const median = sortedNumbers.length % 2 === 0 
         ? (sortedNumbers[sortedNumbers.length / 2 - 1] + sortedNumbers[sortedNumbers.length / 2]) / 2 
         : sortedNumbers[Math.floor(sortedNumbers.length / 2)];
 
+        // Обчислуюю суму відфільтрованих чисел
         const sum = filteredNumbers.reduce((acc, curr) => acc + curr, 0);
+        // Знаходжу середнє арифметичне
         const average = sum / filteredNumbers.length;
 
+        // Створюю змінні для найдовших зростаючих та спадаючих послідовностей
         let longestIncreasingSequence = 1;
         let longestDecreasingSequence = 1;
         let increasingSequence = 1;
         let decreasingSequence = 1;
 
+        // Чере цикл відфілтрованих чисел знаходжу найдовші зростаючі та спадаючі послідовності
         for (let i = 1; i < filteredNumbers.length; i++) {
             if(filteredNumbers[i] > filteredNumbers[i - 1]) {
                 increasingSequence++;
@@ -62,13 +84,14 @@ function handleFile() {
             }
         }
         
+        // Виводжу результат для користувача
         resultsDiv.innerHTML = `<p>Maximum number: ${maxNumber}</p>
                                 <p>Minimum number: ${minNumber}</p>
                                 <p>Median: ${median}</p>
                                 <p>Average: ${average}</p>
                                 <p>Longest increasing sequence: ${longestIncreasingSequence}</p>
                                 <p>Longest decreasing sequence: ${longestDecreasingSequence}</p>`; 
-    }
+    })
 }
 
 
